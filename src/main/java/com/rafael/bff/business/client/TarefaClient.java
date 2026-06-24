9f653b669ccc4c6a0bfadd56d54122b89c0285f3
Rafa-Magalhaes <rafacmagalhaes@hotmail.com>
2026-06-23 23:03:55 -0300

    fix: adicionar valor padrão na URL do TarefaClient para funcionar em testes/CI

--- src/main/java/com/rafael/bff/business/client/TarefaClient.java
+++ src/main/java/com/rafael/bff/business/client/TarefaClient.java
@@ -3,11 +3,12 @@ package com.rafael.bff.business.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "agendador-tarefas", url = "${agendador-tarefas.url}")
+@FeignClient(
+        name = "agendador-tarefas",
+        url = "${agendador-tarefas.url:http://localhost:8081}"
+)
 public interface TarefaClient {
 
-    @GetMapping("tarefas/pendentes")
+    @GetMapping("/tarefas/pendentes")
     void buscarTarefasPendentes();
-
 }
-
