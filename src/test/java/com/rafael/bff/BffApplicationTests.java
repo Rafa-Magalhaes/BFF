e6e20e1269c31b26f54c42aeb8a86eb022aff8c0
Rafa-Magalhaes <rafacmagalhaes@hotmail.com>
2026-06-24 12:12:25 -0300

    test: usar @MockBean no BffApplicationTests para evitar conflito de Feign Client

--- src/test/java/com/rafael/bff/BffApplicationTests.java
+++ src/test/java/com/rafael/bff/BffApplicationTests.java
@@ -1,13 +1,17 @@
 package com.rafael.bff;
 
+import com.rafael.bff.business.client.TarefaClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class BffApplicationTests {
 
+	@MockBean
+	private TarefaClient tarefaClient;
+
 	@Test
 	void contextLoads() {
 	}
-
 }
