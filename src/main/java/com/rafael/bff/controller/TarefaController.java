e3c4a9ca331f26a7f0013730d196d404576eec7a
Rafa-Magalhaes <rafacmagalhaes@hotmail.com>
2026-06-23 22:48:09 -0300

    feat: criar TarefaController com endpoint de teste

--- src/main/java/com/rafael/bff/controller/TarefaController.java
+++ src/main/java/com/rafael/bff/controller/TarefaController.java
@@ -1,4 +1,20 @@
 package com.rafael.bff.controller;
 
+import com.rafael.bff.business.service.TarefaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tarefas")
+@RequiredArgsConstructor
 public class TarefaController {
+
+    private final TarefaService tarefaService;
+
+    @GetMapping("/pendentes")
+    public void buscarTarefasPendentes() {
+        tarefaService.buscarTarefasPendentes();
+    }
 }
