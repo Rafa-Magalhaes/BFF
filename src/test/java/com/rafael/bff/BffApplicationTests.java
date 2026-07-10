e64c51e1114a3e4366edcb8ea86061cc7fcbf1df
Rafa-Magalhaes <rafacmagalhaes@hotmail.com>
2026-07-10 11:29:38 -0300

    test: injeta propriedade do feign client diretamente no SpringBootTest

--- src/test/java/com/rafael/bff/BffApplicationTests.java
+++ src/test/java/com/rafael/bff/BffApplicationTests.java
@@ -1,15 +1,25 @@
 package com.rafael.bff;
 
+import com.rafael.bff.business.client.NotificacaoClient;
 import com.rafael.bff.business.client.TarefaClient;
+import com.rafael.bff.business.client.UsuarioClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"usuario.service.url=http://localhost:8080"
+})
 class BffApplicationTests {
 
 	@MockBean
 	private TarefaClient tarefaClient;
+
+	@MockBean
+	private UsuarioClient usuarioClient;
+
+	@MockBean
+	private NotificacaoClient notificacaoClient;
 
 	@Test
 	void contextLoads() {
