4c5c289be64bc7724db7cbffcb77b7f52e4d6f28
Rafa-Magalhaes <rafacmagalhaes@hotmail.com>
2026-07-22 11:46:13 -0300

    fix: correcao na claim do token de servico e adicao de validacoes nos dtos de request (#10)

--- src/main/java/com/rafael/bff/api/dto/FrontBffAgendamentoRequestDTO.java
+++ src/main/java/com/rafael/bff/api/dto/FrontBffAgendamentoRequestDTO.java
@@ -1,5 +1,8 @@
 package com.rafael.bff.api.dto;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,8 +16,13 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class FrontBffAgendamentoRequestDTO {
 
+    @NotBlank(message = "O título é obrigatório")
     private String titulo;
+
     private String descricao;
+
+    @NotNull(message = "A data e hora são obrigatórias")
+    @Future(message = "A data do agendamento deve ser no futuro")
     private LocalDateTime dataHoraAgendada;
 
 }
