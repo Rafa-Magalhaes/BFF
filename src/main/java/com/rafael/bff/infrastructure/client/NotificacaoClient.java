80a79aa44bfa1d7485119e9b693e4cf0a25b7570
Rafa-Magalhaes <rafacmagalhaes@hotmail.com>
2026-07-29 13:36:54 -0300

    refact: aplica padrao builder nos DTOs e blinda configuracoes do feign client (#13)

--- src/main/java/com/rafael/bff/infrastructure/client/NotificacaoClient.java
+++ src/main/java/com/rafael/bff/infrastructure/client/NotificacaoClient.java
@@ -4,9 +4,11 @@ import com.rafael.bff.infrastructure.clientDTO.BffNotificacaoMailRequestDTO;
 import com.rafael.bff.infrastructure.clientDTO.NotificacaoBffMailResponseDTO;
 import com.rafael.bff.infrastructure.config.ServiceTokenFeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@Validated
 @FeignClient(
         name = "notificacao-service",
         url = "${notificacao.service.url:http://localhost:8083}",
